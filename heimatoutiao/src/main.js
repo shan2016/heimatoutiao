9468dcc0b2e90a5078f208f7e1273d68819396f1
shan <18312015269@163.com>
2019-12-14 18:01:38 +0800

    02-添加登陆的单文件组件并配置好路由，下一步开始制作登陆组件内容
    02-添加登陆的单文件组件并配置好路由，下一步开始制作登陆组件内容

--- heimatoutiao/src/main.js
+++ heimatoutiao/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 
 import '@/styles/reset.css'
 
+import router from '@/router/index.js'
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ heimatoutiao/src/router/index.js
@@ -0,0 +1,18 @@
+// 封装路由
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Login from '@/views/Login.vue'
+Vue.use(VueRouter)
+
+var router = new VueRouter({
+  routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    }
+  ]
+})
+
+export default router
